ee8c19c3b7f2c207b2daae9ee73e7a994b7bd962
AhmedAlSunbati712 <ahmed.m.al.sunbati.27@dartmouth.edu>
2025-12-18 14:16:54 -0500

    gpt api

---
+++ src/api/gpt.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { SERVER_URL } from "@/utils/constants";
+
+
+const extractInfo = async (rawText: string) => {
+    try {
+        const response = await axios.post(`${SERVER_URL}/gpt/extract`, {rawText});
+        const receipt = response.data;
+        return receipt;
+
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
